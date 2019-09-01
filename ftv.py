41d02a8437de59b6b381e26e6d73cd613575cdf8
Florent Kaisser <florent.pro@kaisser.name>
2019-09-01 18:56:16 +0200

    limit to 20 minutes broadcast

--- ftv.py
+++ ftv.py
@@ -14,6 +14,8 @@ APP_VERSION = u'0.2'
 PORT_NUMBER = 58080
 DOMAIN = u'https://www.france.tv'
 
+DURATION_MIN=20
+
 def generate_entry(e,date):
   e=[u'\n<entry>',
     u'\t<title>' + e['title'] + u'</title>',
@@ -44,6 +46,9 @@ def generate_atom(title,link,emissions):
 
 def extract_element(v):
 	content = v.text
+	metadatas = v.find_all('span',class_='c-metadata')
+	date = metadatas[0].text
+	duration = int(metadatas[1].text.split()[0])
 	a = v.find(class_='c-card-video__textarea').h3.a
 	link = a['href']
 	title = a.find('div',class_='c-card-video__title').text
@@ -53,7 +58,7 @@ def extract_element(v):
 	else:
 	  desc = ""
 	uid = a['href']
-	return {"link":link,"title":"%s - %s" % (title,desc),"content":content,"uid":uid}
+	return {"link":link,"title":"%s - %s" % (title,desc),"content":content,"uid":uid,"duration":duration,"date":date}
 	
 def get_emissions(s):
 	return [ extract_element(v) for v in s.find_all("div", class_='c-card-video') ]
@@ -72,7 +77,7 @@ class MyHandler(BaseHTTPRequestHandler):
 			s.send_response(200)
 			s.send_header("Content-type", "application/atom+xml")
 			s.end_headers()
-			s.wfile.write(bytes(generate_atom(title,url,emissions),"utf8"))
+			s.wfile.write(bytes(generate_atom(title,url,filter(lambda e : e['duration'] >= DURATION_MIN, emissions)),"utf8"))
 		except HTTPError:
 			s.send_error(404,"Page not found")
 
